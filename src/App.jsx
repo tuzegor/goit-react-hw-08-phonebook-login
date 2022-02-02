1775867e68d203f9149bcbd7b511d6d866c19a0f
Yehor Tuz <tuzegor@gmail.com>
2022-02-02 22:10:14 +0200

    added logout link

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './views/Login';
 import Header from './components/Header/Header';
 import Register from './views/Register';
 import Contacts from './views/Contacts';
+import authSelectors from './store/auth/authSelectors';
 
 export default function App() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
     <div className="App">
       <Header></Header>
 
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/contacts" component={Contacts} />
+
         <Redirect to="/login" />
       </Switch>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
+import authSelectors from '../../store/auth/authSelectors';
 
 export default function Header() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  console.log(isLoggedIn);
   return (
     <section className={styles.header}>
       <div className="container">
@@ -27,6 +31,14 @@ export default function Header() {
           activeClassName={styles.activeLink}
         >
           Contacts
+        </NavLink>
+        <NavLink
+          // onClick={}
+          to="/login"
+          className={styles.contactsLink}
+          activeClassName={styles.activeLink}
+        >
+          Logout
         </NavLink>
       </div>
     </section>
